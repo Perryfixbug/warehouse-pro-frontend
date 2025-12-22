437a0db9c6d36216b5a1a5920cae93a39a815731
Perryfixbug <hieupmdkim@gmail.com>
2025-12-23 00:14:50 +0700

    feat: protect layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/globals.css'
+import ProtectedLayout from '@/components/layout/protected';
 import Sidebar from '@/components/layout/sidebar';
 import TopBar from '@/components/layout/top-bar';
 
@@ -8,14 +9,16 @@ export default function MainLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className='ml-64 w-[calc(100%-16rem)] h-full'>
-        <TopBar />
-        <div className='mt-18'>
-          {children}
+    <ProtectedLayout>
+      <div className="flex">
+        <Sidebar />
+        <div className='ml-64 w-[calc(100%-16rem)] h-full'>
+          <TopBar />
+          <div className='mt-18'>
+            {children}
+          </div>
         </div>
       </div>
-    </div>
+    </ProtectedLayout>
   )
 }
